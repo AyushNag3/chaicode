a4269782145425105262a2ab0463e88edefbcbad
AyushNag3 <mnag7278@gmail.com>
2025-04-09 23:40:22 +0530

    installing tailwind

---
+++ src/components/whyussection.tsx
@@ -0,0 +1,14 @@
+
+export const Whyus = () => {
+    return (
+        <>
+       
+        <div className="h-screen flex items-center justify-between px-10 text-2xl text-blue-700">
+            <div>01</div>
+            <div>02</div>
+            <div>03</div>
+           
+        </div>
+        </>
+    )
+}
