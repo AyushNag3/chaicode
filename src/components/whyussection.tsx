ea6bc962894c4e477a897062142e6ca64d5fc24c
AyushNag3 <mnag7278@gmail.com>
2025-04-10 14:38:05 +0530

    aaj ka ui

--- src/components/whyussection.tsx
+++ src/components/whyussection.tsx
@@ -4,7 +4,7 @@ export const Whyus = () => {
         <>
         <div className="flex flex-col justify-center ">
           <nav className="w-fit border-b bg-white ">
-      <div className=" px-6 flex  justify-around py-4">
+      <div className=" px-6 flex  justify-around pb-2">
         
         {/* Left Side: Logo or Site Name */}
         <div className="text-3xl text-gray-800 mr-100 pt-4">
@@ -39,9 +39,33 @@ export const Whyus = () => {
       </div>
     </nav>
 
-     <div className="w-80 border rounded-md mt-10 ml-150 py-4 px-2 font-opensans">
+     <div className="w-60 border rounded-md mt-10 ml-155 py-2 px-0 font-opensans">
        <p>Trusted by 1.5M code learners</p>
      </div>
+
+     <div className="w-220 mt-5 ml-80 text-6xl">
+      <p>Consistency and Community Learning for Coding Courses</p>
+     </div>
+
+     <div className="mt-8 ml-100 text-xl flex flex-col items-start">
+      <p className="">Content is everywhere, we provide a learning </p>
+       <p> experience that is unmatched. Bounties, peer learning </p>
+       <p> peer code reviews, Virtual hostel, Alumni Network, Doubt sessions,</p>
+        <p> Group projects and so many other activities to keep you on track. </p> 
+     
+     </div>
+  
+  <div className="flex w-90 gap-4 h-20 py-4 pl-6 mt-12 ml-140 text-3xl border-2 rounded-lg border-amber-400 ">
+      <p>Check all Live Cohorts</p>
+      <div className="flex items-center space-x-2">
+  <span className="relative flex h-3 w-3">
+    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-yellow-500 opacity-75"></span>
+    <span className="relative inline-flex rounded-full h-3 w-3 bg-red-600"></span>
+  </span>
+</div>
+
+  </div>
+
 
     </div>
         </>
